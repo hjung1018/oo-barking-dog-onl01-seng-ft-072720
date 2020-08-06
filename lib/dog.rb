0d0477278a2ef6edfa73ecd35f0be5048fc5e496
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-06 01:48:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,17 @@
 class Dog
   
   def name=(name)
-    name
+    @name = name
   end
   
-  def bark
-    puts "woof"
-  end
+  def name
+    @name
+  end`
   
-end
+#   def bark
+#     puts "woof"
+#   end
+  
+# end
 
 
